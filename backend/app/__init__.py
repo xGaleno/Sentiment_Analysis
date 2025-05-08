f29264106da69dceb9a8be73e0323889679b21bb
xGaleno <diego.degq@gmail.com>
2025-05-08 17:27:37 -0500

    Fix: clean structure and correct imports

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 from .routes import register_routes
 
 def create_app():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    template_dir = os.path.join(base_dir, 'templates')
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    template_dir = os.path.join(base_dir, "templates")
 
     app = Flask(__name__, template_folder=template_dir)
     CORS(app, supports_credentials=True)
